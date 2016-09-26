52e0d9e9d63302bd55438cb7e302abac09192914
my name <myemail@example.com>
2016-09-26 17:19:48 +0300

    h

--- app.js
+++ app.js
@@ -12,6 +12,16 @@ app.get('/', function(req, res){
 	res.end("hello form hey!");
 }); 
 
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
 
 app.use(express.static( __dirname + '/public'));
 
