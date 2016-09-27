bac9d6a74bc281fe55a734957cd76c19049208d3
my name <myemail@example.com>
2016-09-27 20:20:41 +0300

    f

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.static( __dirname + '/public'));
 }) */
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/home.html'));
+    res.sendFile(path.join(__dirname + '/public' + '/home.html'));
 });
 
 
