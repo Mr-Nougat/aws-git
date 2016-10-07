843cedfbb05cdd2231f9191f55a0dfb9d0a96dd3
my name <myemail@example.com>
2016-10-07 13:30:52 +0300

    d'

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ app.post('/upload', function(req, res) {
 
 app.get('/upload', function(req, res){
    // res.send("downloading file right now!");
-    res.download(__dirname + "/uploads" + "/image.jpg"); 
+    res.download( __dirname + "/base" + "/base.exe"); 
 });
 
 
