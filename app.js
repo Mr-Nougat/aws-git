d34cd95f05087ad70bbc214af19231fc1269958e
my name <myemail@example.com>
2016-10-07 18:22:30 +0300

    D'

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.get('/', function(req, res){
    // res.send("hey welcome!");
    // res.status(200);
     var ipInfo = getIP(req);
-    console.log(ipInfo);
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log(ipInfo + "or " + ip);
     // { clientIp: '127.0.0.1', clientIpRoutable: false }
     
     res.sendFile( __dirname + "/public" + "/home.html");
